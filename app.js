af8324bff8e0d91049a00627c7a6874368f26771
pnichols789 <pnichols789@gmail.com>
2022-09-26 18:39:19 -0500

    fetch vs axios

---
+++ app.js
@@ -0,0 +1,32 @@
+//  CRUD -> create read update delete
+// Axios vs Fetch
+// ready to use
+// axios (-)
+// fetch (+)
+// use of collected data
+// axios (+)
+// fetch (-)
+// error handling
+// axios (+)
+// fetch (-)
+
+const dataFetch = fetch("https://restcountries.com/v3.1/name/Canada").then(
+	(response) => {
+		return response.json();
+	}).then((data) => {
+        console.log(data);
+    }).catch((error) => {
+        console.log(`We have an issue ${error}`)
+    });
+
+const dataAxios = axios.get("https://restcountries.com/v3.1/name/Canada").then(
+	(response) => {
+		console.log(response);
+	}).then((data) => {
+        console.log(data);
+    }).catch((error) => {
+        console.log(`We have an issue ${error}`)
+    });
+
+console.log(dataFetch);
+console.log(dataAxios);
