724ea633df5a1ab4dc021f800801ba87455677f2
pnichols789 <pnichols789@gmail.com>
2022-09-26 18:48:02 -0500

    -

--- app.js
+++ app.js
@@ -10,23 +10,31 @@
 // axios (+)
 // fetch (-)
 
-const dataFetch = fetch("https://restcountries.com/v3.1/name/Canada").then(
-	(response) => {
+const dataFetch = fetch("https://restcountries.com/v3.1/name/Canada")
+	.then((response) => {
+		if (!response.ok) {
+			throw Error("Failed");
+		}
 		return response.json();
-	}).then((data) => {
-        console.log(data);
-    }).catch((error) => {
-        console.log(`We have an issue ${error}`)
-    });
+	})
+	.then((data) => {
+		console.log(data);
+	})
+	.catch((error) => {
+		console.log(`We have an issue ${error}`);
+	});
 
-const dataAxios = axios.get("https://restcountries.com/v3.1/name/Canada").then(
-	(response) => {
+const dataAxios = axios
+	.get("https://restcountries.com/v3.1/name/Canada")
+	.then((response) => {
 		console.log(response);
-	}).then((data) => {
-        console.log(data);
-    }).catch((error) => {
-        console.log(`We have an issue ${error}`)
-    });
+	})
+	.then((data) => {
+		console.log(data);
+	})
+	.catch((error) => {
+		console.log(`We have an issue ${error}`);
+	});
 
 console.log(dataFetch);
 console.log(dataAxios);
